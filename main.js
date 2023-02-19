498241a95cf5096a773ccf01805fca07f6939ae3
Eliel-Carvalho <elielcarvalh0605@gmail.com>
2023-02-19 12:24:46 -0300

    Comecei os primeiros scrips

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import './style.css'
 
+let usermail = document.querySelector("#email")
+let userPassword = document.querySelector("#password")
 
+let submit = document.querySelector("#signIn")
+
+submit.addEventListener("click", (i) => {
+  i.preventDefault()
+
+  checkInput()
+})
+
+function checkInput()  {
+
+}
