ec3ae3f9d678b0b6fad55c1a9565f4dabb1d67a1
Eliel-Carvalho <elielcarvalh0605@gmail.com>
2023-02-20 23:31:45 -0300

    As primeiras emplementações com o javascript, ja foram adc, mas o codigo estar bugado!

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 import './style.css'
 
-let usermail = document.querySelector("#email")
+let userEmail = document.querySelector("#email")
 let userPassword = document.querySelector("#password")
-
 let submit = document.querySelector("#signIn")
+
+const setError = (input, message) => {
+  const InputBox = input.parentElement
+  const small = InputBox.querySelector("small")
+
+  InputBox.className = "form input-group input-box error"
+  small.innerText = message
+
+}
+
+const setSeccess = (input) => {
+  const InputBox = input.parentElement
+
+  InputBox.className = "form input-group input-box success"
+}
+
+const checkInput = () => {
+  if(userEmail === ""){
+    setError(userEmail, "Digite um E-mail valido")
+  }else{
+    setSeccess(userEmail)
+  }
+}
+
 
 submit.addEventListener("click", (i) => {
   i.preventDefault()
 
   checkInput()
 })
-
-function checkInput()  {
-
-}
