95fc37ef6169a9e64a4798a15bd0ed7ef8baf858
Eliel-Carvalho <elielcarvalh0605@gmail.com>
2023-02-27 20:34:46 -0300

    finalizado as validações

--- main.js
+++ main.js
@@ -4,32 +4,38 @@ let userEmail = document.querySelector("#email")
 let userPassword = document.querySelector("#password")
 let submit = document.querySelector("#signIn")
 
-const setError = (input, message) => {
+
+submit.addEventListener("click", (i) => {
+  i.preventDefault()
+  
+  checkInput()
+})
+
+function checkInput() {
+  if(userEmail.value === ""){
+    setError(userEmail, "Digite um E-mail valido!")
+  }else{
+    setSeccess(userEmail)
+  }
+
+  if(userPassword.value === ""){
+    setError(userPassword, "Digite uma senha valida!")
+  }else{
+    setSeccess(userPassword)
+  }
+}
+
+function setError(input, message){
   const InputBox = input.parentElement
   const small = InputBox.querySelector("small")
 
-  InputBox.className = "form input-group input-box error"
+  InputBox.className = "input-box error"
   small.innerText = message
 
 }
 
-const setSeccess = (input) => {
+function setSeccess (input) {
   const InputBox = input.parentElement
 
-  InputBox.className = "form input-group input-box success"
+  InputBox.className = "input-box success"
 }
-
-const checkInput = () => {
-  if(userEmail === ""){
-    setError(userEmail, "Digite um E-mail valido")
-  }else{
-    setSeccess(userEmail)
-  }
-}
-
-
-submit.addEventListener("click", (i) => {
-  i.preventDefault()
-
-  checkInput()
-})
